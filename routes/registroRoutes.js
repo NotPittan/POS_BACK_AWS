7c2fbb33a780b1b9b99e7b34da8af68640c55275
NotPittan <pedroenocgb1245@gmail.com>
2026-03-01 14:46:52 -0500

    registro_back_hotfix

--- routes/registroRoutes.js
+++ routes/registroRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const registroController = require("../controllers/registroController");
+const registroController = require("../controllers/registrosController");
 
 // Ruta: POST /api/daily-records/bulk
 router.post("/bulk", registroController.procesarCierreDiario);
